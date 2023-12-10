73460708bfae1c6a1c252d17294c4661cf4bb300
MuskanMall <muskanmall22@gmail.com>
2023-12-10 18:20:54 -0500

    converting home page to index page

--- route.py
+++ route.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = '6b02adc4284f85698f6f61abe5fca402'
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('index.html', title='Home')
 
 
 @app.route('/capture', methods=['GET','POST'])
